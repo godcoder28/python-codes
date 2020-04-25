6bee9c653a9b042bfac7e37b7f7037299a939e7f
sg287 <shivanggupta287@gmail.com>
2020-04-25 16:18:30 +0530

    oops test

---
+++ oops.py
@@ -0,0 +1,18 @@
+from tkinter import *
+"""class window():
+    def __init__(self, win_nm, geo, title):
+        self.win_nm=Tk()
+        self.win_nm.title(title)
+        self.win_nm.geometry(geo)
+    def runw(self):
+        self.win_nm.mainloop()
+W1= window('1','400x180','hello')
+W2= window('2','440x440','123456789')
+W1.runw()"""
+w1=Tk()
+w1.geometry('440x400')
+w1.title('w1')
+
+w=Tk()
+w.geometry('440x400')
+w.mainloop()
