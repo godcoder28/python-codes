109d231e7eb319c0477bea32be2b940433ac243e
shivang <shivanggupta287@gmail.com>
2020-08-04 12:55:40 +0530

    changes

--- file handeling.py
+++ file handeling.py
@@ -1,4 +1,11 @@
-file=open('demo.txt','r+')
-print(file.read())
+file = open('xy.dat', 'r+')
+l = file.read()
+l2 = l.replace(' ', '\n').split('\n')
+print(l2)
 
 
+def DISPLAYWORDS(m):
+    for i in m:
+        print(i, end=' ') if len(i) <= 4 else print(end='')
+
+DISPLAYWORDS(l2)
